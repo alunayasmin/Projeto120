0dd310fe2de60027fc8a76fdca0a87a56121b3c2
Yasmin <125517021+alunayasmin@users.noreply.github.com>
2023-10-02 19:53:51 -0300

    Add files via upload

---
+++ mainn.js
@@ -0,0 +1,20 @@
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+
+Webcam.set({
+    with:350,
+    heitght:300,
+    image_format : 'png',
+    png_quality:90
+})
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '"<img id=selfie_image" src="'+data_uri+'"/>';
+    });
+
+    console.log('ml5 version;', ml5.version);
+}
+
